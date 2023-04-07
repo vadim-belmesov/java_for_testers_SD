cfda7fdbc4c9ab2c9cc94683c5b9736ce5c722d9
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-04-07 18:47:24 +0300

    for lecture about operations

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/operation/math_operations.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox.operation;
+
+public class math_operations {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2); //result == 0!
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+    }
+}
