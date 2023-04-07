72a14c1e5cc8848834524d485196756b20eccbde
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-04-07 18:59:58 +0300

    var_type

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/var_type/vars.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox.var_type;
+
+public class vars {
+
+    public static void main(String[] args) {
+        String some_string = "world";
+        System.out.println("Hello, "+ some_string + "!");
+
+        double l = 8;
+        double s = l + l;
+        System.out.println("S = " + l + " = " + s);
+    }
+}
