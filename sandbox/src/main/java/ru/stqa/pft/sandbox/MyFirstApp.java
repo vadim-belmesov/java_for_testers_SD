c86a398b4778eab1d08a354746b063b298244b17
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-04-11 07:26:48 +0300

    Изучаем классы, объекты и их методы/экземпляры
    К лекции 1.8

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApp.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApp.java
@@ -3,7 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //Описание объектов
+        Square s = new Square(5); //создаём объект s типа Square
+//        s.l = 5;актуально, когда не используется конструктор
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    }        
+        Rectangle r = new Rectangle(2, 3); //создаём объект r типа Rectangle
+//        r.a = 2; актуально, когда не используется конструктор
+//        r.b = 3;актуально, когда не используется конструктор
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна = " + area(r));
+    }
+    //Ф-я Вычисление площади квадрата
+    public static double area(Square s){
+        return s.l * s.l;
+    }
+    //Ф-я  вычисление площади рямоугольнка
+    public static double area(Rectangle r){
+        return r.a * r.b;
+    }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double len){
+    this.l = len;
+  }//конструктор this.l - ссылается на внешний атрибут-переменную l
+}
