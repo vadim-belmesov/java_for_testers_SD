266828e7461ec204f5d2713e3e840f5eddfdd05b
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-04-14 17:08:53 +0300

    Переделал тест согласно правкам в методе и классе для задачи измерения дистанции меж точками

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTest.java
@@ -7,25 +7,27 @@ import ru.stqa.pft.sandbox.point_distace.Point;
 public class PointDistanceTest {
   @Test
   public void DistanceTest_X2Y2(){
-    double x1=8;
-    double y1=-1;
-    Point p = new Point(x1,y1);
+    double x1=155;
+    double y1=-100;
+    Point p1 = new Point(x1,y1);
 
     double x2=4;
     double y2=2;
+    Point p2 = new Point(x2,y2);
 
-    Assert.assertEquals(p.p2(x2,y2),50);
+    Assert.assertEquals(p1.distance(p2),182.22239159883728);
 
   }
   @Test
   public void DistanceTest_X1Y1(){
     double x1=8;
     double y1=-1;
-    Point p = new Point(x1,y1);
+    Point p1 = new Point(x1,y1);
 
     double x2=4;
     double y2=2;
+    Point p2 = new Point(x2,y2);
 
-    Assert.assertEquals(p.p2(x2,y2),5.0);
+    Assert.assertEquals(p1.distance(p2),5.0);
   }
 }
