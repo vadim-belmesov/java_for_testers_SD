98349105340d631eaf4cd43f07abd955df3a4e6f
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-04-30 14:56:51 +0300

    Переименовал CreateContactTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.*;
 
-public class CreateContact extends TestBase{
+public class CreateContactTest extends TestBase{
 
   @Test
   public void testCreateContact() throws Exception {
