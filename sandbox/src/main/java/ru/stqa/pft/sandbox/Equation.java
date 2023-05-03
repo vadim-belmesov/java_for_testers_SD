ad1b67ca5b1143ad726d01a02ccab340110b547f
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-05-03 17:07:45 +0300

    Оптимизировал код для if else

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -15,25 +15,23 @@ public class Equation {
 
         double d = b * b - 4 * a * c;
 
-        if (a == 0) {
-          if (b == 0) {
-            if (c == 0) {
-                n = -1;
-            } else {
-                n = 0;
-            }
-          } else {
-              n = 1;
-          }
+        if (a != 0) {
+         if (d > 0) {
+            n = 2;
+        } else if (d == 0) {
+            n = 1;
+        } else {
+            n = 0;
+        }
+
+        } else if (b != 0) {
+            n = 1;
+
+        } else if (c != 0) {
+            n = 0;
 
         } else {
-          if (d > 0) {
-              n = 2;
-          } else if (d == 0) {
-              n = 1;
-          } else {
-              n = 0;
-          }
+            n = -1;
         }
     }
 
