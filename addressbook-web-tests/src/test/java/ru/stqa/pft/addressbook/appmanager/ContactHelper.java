cd81568ccef3e3904acd0c491b2f0956eefc1130
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-05-03 15:01:20 +0300

    Добавил специализированный метод selectContact() в ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactHelper extends HelperBase {
     public ContactHelper(WebDriver wd) {
@@ -27,5 +26,9 @@ public class ContactHelper extends HelperBase {
 
     public void deleteContactButton() {
         click(By.xpath("//input[@value='Delete']"));
+    }
+
+    public void selectContact() {
+        click(By.name("selected[]"));
     }
 }
