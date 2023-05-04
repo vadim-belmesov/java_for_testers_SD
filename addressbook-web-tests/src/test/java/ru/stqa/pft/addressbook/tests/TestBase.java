38c7343438be9a11543c83bf0a738843cf6c7cfb
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-05-04 13:54:50 +0300

    Добавил усовие для запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(Browser.IE.browserName());
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
