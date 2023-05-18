829cda90e302bb7c8f649a66928a7fee40df879e
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2023-05-18 18:28:14 +0300

    Оптимизировал contactDeletetion на случай отсутствия объекта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletetionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletetionTests.java
@@ -1,15 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactDeletetionTests extends TestBase{
+public class ContactDeletetionTests extends TestBase {
 
-    @Test
-    public void testContactDeletetion(){
-        app.getNavigationHelper().goToHomePage();
-        app.getContactHelper().selectContact();
-        app.getContactHelper().deleteContactButton();
-        app.getContactHelper().closeAlert();
-
+  @Test
+  public void testContactDeletetion() {
+    app.getNavigationHelper().goToHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().goToNewContactPage();
+      app.getContactHelper().createContact(new ContactData("1", "2", "+7", "a@a.com", "Test11"), true);
+      app.getNavigationHelper().goToHomePage();
     }
+    app.getContactHelper().selectContact();
+    app.getContactHelper().deleteContactButton();
+    app.getContactHelper().closeAlert();
+
+  }
 }
